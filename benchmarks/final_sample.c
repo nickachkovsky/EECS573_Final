2614ae7949054cb92a830454da4c98bb19b8f041
Sahil Surapaneni <ssurapan@eecs583a.eecs.umich.edu>
2022-12-07 18:03:59 -0500

    final_sample byte code

---
+++ benchmarks/final_sample.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 10;
+    int y = x + 5;
+
+    int z = 15;
+    if (y == z)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
